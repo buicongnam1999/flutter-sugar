0729a301bf7b91208b6c13876345e1cb3fd5f320
lletuan <lletuan@redseal.net>
2023-01-12 17:22:12 +0700

    create project

---
+++ flutter_application/lib/controller/BaseController.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+export 'package:get/get.dart';
+import 'package:google_mobile_ads/google_mobile_ads.dart';
+
+class BaseController extends GetxController {
+
+}
